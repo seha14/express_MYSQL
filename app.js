8e834b3db1db3195baa0d025a31efcf07b00aeac
seha14 <sehabuddinnasution@gmail.com>
2019-07-30 11:08:33 +0700

    express_MYSQL

---
+++ app.js
@@ -0,0 +1,60 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const expressValidator = require('express-validator');
+const flash = require('express-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const mysql = require('mysql');
+const connection  = require('./lib/db');
+
+const indexRouter = require('./routes/index');
+//const usersRouter = require('./routes/users');
+const customersRouter = require('./routes/customers');
+
+const app = express();
+
+
+const {getHomePage} = require('./routes/index');
+// view engine setup
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'ejs');
+
+ app.use(logger('dev'));
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({ extended: true }));
+ app.use(cookieParser());
+ app.use(express.static(path.join(__dirname, 'public')));
+
+ app.use(session({ 
+     secret: '123456cat',
+     resave: false,
+     saveUninitialized: true,
+     cookie: { maxAge: 60000 }
+ }))
+
+ app.use(flash());
+ //app.use(expressValidator());
+
+ app.use('/', indexRouter);
+ //app.use('/users', usersRouter);
+ app.use('/customers', customersRouter);
+
+ // catch 404 and forward to error handler
+ app.use(function(req, res, next) {
+   next(createError(404));
+ });
+
+ // error handler
+ app.use(function(err, req, res, next) {
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
+ // render the error page
+   res.status(err.status || 500);
+   res.render('error');
+ });
+ module.exports = app;
